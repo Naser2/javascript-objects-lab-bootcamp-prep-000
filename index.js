5d821b422def2e52801d4d80623e9535722316bd
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 20:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   const item = Object.assign({}, object)
-  return delete Item[key]
+  return delete item[key]
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
